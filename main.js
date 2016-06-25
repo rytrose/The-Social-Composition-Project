b72a02ad83584aed312dd63f8cfa28f820d14377
Ryan Rose <rytrose@gmail.com>
2016-06-24 20:45:17 -0400

    working prototype with sync'd visualization!

--- main.js
+++ main.js
@@ -27,6 +27,8 @@ var reactionArrayofArrays = [];
 var indexOfLongest = 0;
 var likeCount = 0;
 
+var rFlag = -1;
+
 // Facebook API Init
 window.fbAsyncInit = function() {
   FB.init({
@@ -191,14 +193,14 @@ function nextReactionsPage(response, callback, nextURL){
 }
 
 function finish(){
-  var length = reactionArrayofArrays[0].length;
+  var length = 0;
   for(i = 0; i < reactionArrayofArrays.length; i++){
-    if(reactionArrayofArrays[i].length > length){
+    if(reactionArrayofArrays[i].length > length && reactionArrayofArrays[i].length < 50){
       indexOfLongest = i;
       length = reactionArrayofArrays[i].length;
     }
   }
-  debugger;
+
   console.log(reactionArrayofArrays[indexOfLongest]);
   var div = document.getElementById("loading");
   div.style.display = "none";
@@ -223,10 +225,23 @@ function CompositionGeneration(context){
 }
 
 CompositionGeneration.prototype.playComposition = function(){
+  // Start animation
+  animation();
+
   var time = context.currentTime;
   var quarterNote = 0.5357143; // quarter note = 112bpm
-
   var intro = 16 * quarterNote;
+
+  var highlightViz = function( ind ){
+    rFlag = ind;
+    if (ind < reactionArrayofArrays[indexOfLongest].length - 1){
+      setTimeout(function(){highlightViz(ind + 1)}, Math.floor(1000 * quarterNote * 8))
+    }
+  }
+
+  setTimeout(function(){highlightViz(0)},  Math.floor(1000 * intro));
+
+
   var droneSource0 = this.makeDroneSource(this.buffers[1]);
   droneSource0[droneSource0.start ? 'start' : 'noteOn'](time);
 
@@ -288,5 +303,207 @@ CompositionGeneration.prototype.makeDroneSource = function(buffer) {
  * XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
  */
 function animation(){
+  var camera, scene, renderer;
+  var controls;
 
+  var particlesTotal = 512;
+  var positions = [];
+  var objects = [];
+  var current = 0;
+  var names = nameArrayofArrays[indexOfLongest];
+  var reactions = reactionArrayofArrays[indexOfLongest];
+
+  var ind = 0;
+
+  init();
+  animate();
+
+  function init() {
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 5000 );
+    camera.position.set( 2000, 1600, 3000 );
+    camera.target = new THREE.Vector3();
+    camera.lookAt( new THREE.Vector3() );
+
+    scene = new THREE.Scene();
+
+    var image = document.createElement( 'img' );
+    image.addEventListener( 'load', function ( event ) {
+
+      var radius = reactions.length * 5;
+
+      for ( var i = 0; i < reactions.length; i ++ ) {
+        var sprite = image.cloneNode();
+        if(reactions[i] == "LIKE")
+          sprite.src = 'Facebook Reaction PNGs/like.png';
+        if(reactions[i] == "LOVE")
+          sprite.src = 'Facebook Reaction PNGs/love.png';
+        if(reactions[i] == "SAD")
+          sprite.src = 'Facebook Reaction PNGs/sad.png';
+        if(reactions[i] == "ANGRY")
+          sprite.src = 'Facebook Reaction PNGs/angry.png';
+        if(reactions[i] == "HAHA")
+          sprite.src = 'Facebook Reaction PNGs/haha.png';
+        sprite.title = names[i];
+        var object = new THREE.CSS3DSprite( sprite );
+
+        var phi = Math.acos( -1 + ( 2 * i ) / reactions.length );
+        var theta = Math.sqrt( reactions.length * Math.PI ) * phi;
+
+        object.position.x = radius * Math.cos( theta ) * Math.sin( phi ),
+        object.position.y = radius * Math.sin( theta ) * Math.sin( phi ),
+        object.position.z = radius * Math.cos( phi );
+        scene.add( object );
+
+        objects.push( object );
+
+      }
+
+      transition();
+
+    }, false );
+    image.src = 'Facebook Reaction PNGs/like.png';
+
+    // Random
+
+    for ( var i = 0; i < reactions.length; i ++ ) {
+
+      positions.push(
+        Math.random() * 4000 - 2000,
+        Math.random() * 4000 - 2000,
+        Math.random() * 4000 - 2000
+      );
+
+    }
+
+    // Sphere
+
+    var radius = 500;
+
+    for ( var i = 0; i < reactions.length; i ++ ) {
+
+      var phi = Math.acos( -1 + ( 2 * i ) / reactions.length );
+      var theta = Math.sqrt( reactions.length * Math.PI ) * phi;
+
+      positions.push(
+        radius * Math.cos( theta ) * Math.sin( phi ),
+        radius * Math.sin( theta ) * Math.sin( phi ),
+        radius * Math.cos( phi )
+      );
+
+    }
+
+    //
+
+    renderer = new THREE.CSS3DRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.domElement.style.position = 'absolute';
+    document.getElementById( 'container' ).appendChild( renderer.domElement );
+
+    //
+
+    controls = new THREE.TrackballControls( camera, renderer.domElement );
+    controls.rotateSpeed = 2.0;
+    controls.zoomSpeed = 0.8;
+    controls.panSpeed = 0.4;
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    window.scrollTo(0,356);
+  }
+
+  function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+  }
+
+  function transition() {
+    var offset = current * reactions.length * 3;
+    var duration = 2000;
+
+    for ( var i = 0, j = offset; i < reactions.length; i ++, j += 3 ) {
+
+      var object = objects[ i ];
+      var tween1 = new TWEEN.Tween( object.position )
+          .to( {
+            x: positions[ j ],
+            y: positions[ j + 1 ],
+            z: positions[ j + 2 ]
+          }, Math.random() * duration + duration )
+          .easing( TWEEN.Easing.Exponential.InOut )
+          .start();
+    }
+    var tween2 = new TWEEN.Tween( this )
+      .to( {}, duration * 2 )
+      .start();
+    current = ( current + 1 ) % 2;
+  }
+
+  var test1 = function () {
+    var img = document.createElement( 'img' );
+    if(reactions[rFlag] == "LIKE")
+      img.src = 'Facebook Reaction PNGs/like1.png';
+    if(reactions[rFlag] == "LOVE")
+      img.src = 'Facebook Reaction PNGs/love1.png';
+    if(reactions[rFlag] == "SAD")
+      img.src = 'Facebook Reaction PNGs/sad1.png';
+    if(reactions[rFlag] == "ANGRY")
+      img.src = 'Facebook Reaction PNGs/angry1.png';
+    if(reactions[rFlag] == "HAHA")
+      img.src = 'Facebook Reaction PNGs/haha1.png';
+    img.title = names[rFlag];
+    var obj = new THREE.CSS3DSprite( img );
+    obj.position.x = objects[rFlag].position.x;
+    obj.position.y = objects[rFlag].position.y;
+    obj.position.z = objects[rFlag].position.z;
+    objects[rFlag] = obj;
+    var toRemove = scene.children[0];
+    var name = scene.children[0].element.title;
+
+    var number = document.createElement( 'div' );
+    number.className = 'label';
+    number.textContent = name;
+    label = new THREE.CSS3DObject( number );
+    label.position.x = objects[rFlag].position.x;
+    label.position.y = objects[rFlag].position.y - 75;
+    label.position.z = objects[rFlag].position.z;
+    scene.remove(toRemove);
+    scene.add(obj);
+    scene.add(label);
+
+    console.log("changed src");
+  }
+
+  function animate() {
+
+    requestAnimationFrame( animate );
+
+    TWEEN.update();
+
+    controls.update();
+
+    if(rFlag > -1){
+      test1();
+      rFlag = -1;
+    }
+
+    var time = performance.now();
+
+    for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+      var object = objects[ i ];
+      var scale = Math.sin( ( Math.floor( object.position.x ) + time ) * 0.002 ) * 0.3 + 1;
+      object.scale.set( scale, scale, scale );
+
+    }
+
+    renderer.render( scene, camera );
+
+  }
 }
